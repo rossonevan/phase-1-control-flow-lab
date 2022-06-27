b83e76f0b70ade1bd55fa2a5e75f0d436024341d
Evan Rosson <rossonevan7281@gmail.com>
2022-06-27 17:15:46 -0500

    commit tree b0f17a3b1754f2f66cf885cbd7920be74b4830c0

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  let result;
+  if (someValue <= 400) {
+    result = "This one is on me!";
+  }
+  else if (someValue > 400 && someValue <= 2000 ) {
+    result = 'That will be twenty bucks.';
+  }
+  else if (someValue > 2000 && someValue <= 2500) {
+    result = 'I will gladly take your thirty bucks.';
+  }
+  else if (someValue > 2500 ) {
+    result = 'No can do.';
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let result;
+  if (city === "NYC") {
+  result = "Ok, sounds good.";
+  }
+  else {
+    result = "No go.";
+  }
+  return result;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(input){
+  let result;
+  switch(input) {
+    case 'generous':
+      result = "Thank you so much."
+      break;
+    case 'not as generous':
+      result = "Thank you."
+      break;
+    default:
+      result = "Bye."
+      break;
+  }
+  return result;
 }
